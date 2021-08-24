ea122835dcbe9b26f4aaecfa535f7c4b4c8f76a9
Adrian Lucas <ad16deg@gmail.com>
2021-08-23 21:06:12 -0600

    agregue toma de muestra por tiempo en comuniciacion.py

--- comunicacion.py
+++ comunicacion.py
@@ -6,7 +6,13 @@ time.sleep(2)
 
 # GPIO.setmode(GPIO.BOARD)
 # GPIO.setup(7, GPIO.OUT)
+inicio = time.time()
+tiempo = 0
 muestra = []
-while(True):
+while(tiempo<5):    #toma muestras por 5 sec
     rawString = arduino.readline()
     print(rawString) 
+    muestra.append(rawString)
+    tiempo = time.time()-inicio
+
+print(muestra)
