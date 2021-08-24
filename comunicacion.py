20c7867541fe481da8721a766b2457031ac15a15
Adrian Lucas <ad16deg@gmail.com>
2021-08-23 21:17:01 -0600

    contador y asignacion a lista en comunicacio.py

--- comunicacion.py
+++ comunicacion.py
@@ -1,18 +1,18 @@
 import serial, time
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
-arduino = serial.Serial('/dev/ttyACM0',9600)
+arduino = serial.Serial('/dev/ttyACM0',9600)    #cambiar el string por el nombre del puerto conectado al arduino
 time.sleep(2)
 
 # GPIO.setmode(GPIO.BOARD)
 # GPIO.setup(7, GPIO.OUT)
 inicio = time.time()
 tiempo = 0
-muestra = []
+muestras = []
 while(tiempo<5):    #toma muestras por 5 sec
     rawString = arduino.readline()
-    print(rawString) 
-    muestra.append(rawString)
+    print(rawString)
+    muestras.append(int(rawString.decode('utf-8')))
     tiempo = time.time()-inicio
 
-print(muestra)
+print(muestras)
